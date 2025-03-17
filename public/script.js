f2cc66295e8a898c31df547c5f05e9885b5ad548
tubaali3167@gmail.com <ta6402792@gmail.com>
2025-03-18 00:07:09 +0500

    cors resolved

--- public/script.js
+++ public/script.js
@@ -24,6 +24,7 @@ document.getElementById('downloadBtn').addEventListener('click', async () => {
 			url: 'https://instagram-reels-downloader-api.p.rapidapi.com/download',
 			params: {
 				userId: '25025320',
+				url:videoUrl
 			},
 
 			headers: {
