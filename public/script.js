3e7c9db8ee0ed0d6c6df16cdc92d7106100539dd
tubaali3167@gmail.com <ta6402792@gmail.com>
2025-03-17 23:54:43 +0500

    fixed params

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ document.getElementById('downloadBtn').addEventListener('click', async () => {
 
 	try {
 		// Pass the videoUrl as a query parameter
-		const fullUrl = `https://instagram-reels-downloader-api.p.rapidapi.com/download?url=${encodeURIComponent(
+		const fullUrl = `https://instagram-reels-downloader-api.p.rapidapi.com/download?userId=25025320&url=${encodeURIComponent(
 			videoUrl,
 		)}`;
 		console.log(fullUrl);
